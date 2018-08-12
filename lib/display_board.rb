8b6768b9caaf7c84e115a1083c976d2d93f50000
Ashley Bland <ashleyjbland@gmail.com>
2018-08-12 15:28:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,12 +2,12 @@
 # out the current state.
 board = ["X", " ", " ", " ", " ", " ", " ", " ", " ",]
 
-def display_board
+def display_board(board = "X")
   puts " #{board[0]} |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "
 end
 
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " ",]
 
-def display_board
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", "   |   |   ", "-----------", "   |   |   "
 end
